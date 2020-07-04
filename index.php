cdc431839c474eba53c9198ff8aa0131c248683f
Eliezer Lopez Sandoval <371EZE6.375@GMAIL.COM>
2020-07-04 00:41:00 -0500

    sdsd

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<title>HI!</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.min.css"/>
+		<style>
+			html {
+				background: #C405F8;
+				color: #F8BD05;
+				font-family: monospace;
+				font-size: 20px;
+				padding: 10px 20px;
+			}
+			h1 {
+				font-weight: 300;
+			}
+			#container {
+				display: flex;
+				list-style: none;
+				flex-wrap: wrap;
+			}
+			a {
+				display: block;
+				padding: 15px 0;
+			}
+			img  {
+				max-height: 400px;
+				max-width: 380px;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>HOLA BONITA!</h1>
+
+		<ul id="container">
+			<li><a href="cup/index.html"><img src="img/rrr.Jpg"></a></li>
+<h5>Presiona la imagen</h5>
+		</ul>
+		
+	<div>
+		<h2><h1>TE AMOOOOOO!!! KARINA VASQUEZ...</h1></h2>
+	</div>
+		<div class="github-fork-ribbon-wrapper right">
+			<div class="github-fork-ribbon" style="background: #8AACFF">
+				<a href="https://github.com/ngokevin/aframe-blender-projects">TE AMOOOO!!!MI KARY</a>
+			</div>
+		</div>
+	</body>
+</html>
